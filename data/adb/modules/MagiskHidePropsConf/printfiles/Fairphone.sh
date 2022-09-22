5c6aad8b07d98cf3c3666e9c8182e27476d23c78
ShaDisNX255 <rayo_dis_nx@hotmail.com>
2022-09-21 21:01:57 -0500

    Removing MagiskHide Props
    No longer needed, fixing with default.prop

--- data/adb/modules/MagiskHidePropsConf/printfiles/Fairphone.sh
+++
@@ -1,5 +0,0 @@
-PRINTSLIST="
-Fairphone 2 (6.0.1):Fairphone:FP2=Fairphone/FP2/FP2:6.0.1/FP2-gms-17.09.3/FP2-gms-17.09.3:user/release-keys
-Fairphone 3/3 Plus (10):Fairphone:FP3=Fairphone/FP3/FP3:10/8901.3.A.0054.20200929/09290922:user/release-keys__2020-10-05
-"
-BASICATTMODEL="FP2"
